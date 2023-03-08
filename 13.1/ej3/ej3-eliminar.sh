7d93f8b8f7a52fd5dfaa6be7b7d62a715dfe1a1d
Guille <gmonmar2408@g.educaand.es>
2023-03-08 12:32:09 +0100

    Fix eliminación

--- 13.1/ej3/ej3-eliminar.sh
+++ 13.1/ej3/ej3-eliminar.sh
@@ -16,34 +16,34 @@ INSTANCE_NAME_NFS=nfs
 # Eliminar Backend
 aws ec2 terminate-instances \
     --instance-ids $(aws ec2 describe-instances \
-                    --filters "Name=$INSTANCE_NAME_BACKEND,Values=running" \
+                    --filters "Name=tag:Name,Values=$INSTANCE_NAME_BACKEND" \
                     --query "Reservations[*].Instances[*].InstanceId" \
                     --output text)
 
 # Eliminar frontend 1
 aws ec2 terminate-instances \
     --instance-ids $(aws ec2 describe-instances \
-                    --filters "Name=$INSTANCE_NAME_FRONTEND_01,Values=running" \
+                    --filters "Name=tag:Name,Values=$INSTANCE_NAME_LOAD_BALANCER" \
                     --query "Reservations[*].Instances[*].InstanceId" \
                     --output text)
 
 # Eliminar frontend 2
 aws ec2 terminate-instances \
     --instance-ids $(aws ec2 describe-instances \
-                    --filters "Name=$INSTANCE_NAME_FRONTEND_02,Values=running" \
+                    --filters  "Name=tag:Name,Values=$INSTANCE_NAME_FRONTEND_01" \
                     --query "Reservations[*].Instances[*].InstanceId" \
                     --output text)
 
 # Eliminar load balancer
 aws ec2 terminate-instances \
     --instance-ids $(aws ec2 describe-instances \
-                    --filters "Name=$INSTANCE_NAME_LOAD_BALANCER,Values=running" \
+                    --filters  "Name=tag:Name,Values=$INSTANCE_NAME_FRONTEND_02" \
                     --query "Reservations[*].Instances[*].InstanceId" \
                     --output text)
 
 # Eliminamos NFS
 aws ec2 terminate-instances \
     --instance-ids $(aws ec2 describe-instances \
-                    --filters "Name=$INSTANCE_NAME_NFS,Values=running" \
+                    --filters  "Name=tag:Name,Values=$INSTANCE_NAME_NFS" \
                     --query "Reservations[*].Instances[*].InstanceId" \
                     --output text)
